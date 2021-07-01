28183498b6311a31e04762de4660ccd8f71b8a97
phat.dang <phat.dang@pap-tech.com>
2021-07-01 09:02:49 +0700

    Add project files.

---
+++ core-ecommerce.EntityFrameworkCore/EntityFramework/EcommerceContextFactory.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace core_ecommerce.EntityFrameworkCore.EntityFramework
+{
+    public class EcommerceContextFactory : IDesignTimeDbContextFactory<EcommerceDbContext>
+    {
+        public EcommerceDbContext CreateDbContext(string[] args)
+        {
+            //read configuration from appsettings.js
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            var connectionString = configuration.GetConnectionString("EcommerceDatabase");
+
+            //create new instance and inject database driver configuration
+            var optionsBuilder = new DbContextOptionsBuilder<EcommerceDbContext>();
+            optionsBuilder.UseNpgsql(connectionString);
+
+            return new EcommerceDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ core-ecommerce.EntityFrameworkCore/EntityFramework/EcommerceDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace core_ecommerce.EntityFrameworkCore.EntityFramework
+{
+    public class EcommerceDbContext : DbContext
+    {
+        public EcommerceDbContext(DbContextOptions options) : base(options)
+        {
+            //none
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //Config
+
+            //seed
+
+            //parent
+            //base.OnModelCreating(modelBuilder);
+        }
+
+        //Define DbSet
+    }
+}
